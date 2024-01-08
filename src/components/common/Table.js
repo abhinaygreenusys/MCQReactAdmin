ad2b36a6c01296abff26181ee06e4fa3970c52d6
Ashutosh <ashutoshks@digitalnightowl.in>
2024-01-08 16:55:34 +0530

    Add Table component and its CSS styles

---
+++ src/components/common/Table.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./table.css";
+
+const Table = ({
+  tHead = [],
+  wrapperClass = "",
+  loading = "false",
+  children,
+}) => {
+  return (
+    <div className={`w-full overflow-x-auto ${wrapperClass}`}>
+      <table className="w-full">
+        <thead>
+          <tr>
+            {tHead.map((item, index) => (
+              <th key={index}>{item}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {loading ? (
+            <tr>
+              <td colSpan={tHead.length} className="text-center">
+                Loading...
+              </td>
+            </tr>
+          ) : (
+            children
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
