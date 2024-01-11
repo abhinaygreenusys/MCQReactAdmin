733b8c22043358c529467dd7482f1b63d2491b18
Ashutosh <ashutoshks@digitalnightowl.in>
2024-01-11 14:58:19 +0530

    Refactor ModeratorList component

--- src/pages/ModeratorList/index.js
+++ src/pages/ModeratorList/index.js
@@ -10,15 +10,12 @@ import { RiDeleteBinLine } from "react-icons/ri";
 const ModeratorList = () => {
   const [loading, setLoading] = useState(true);
   const [moderators, setModerators] = useState([]);
-  const [totalPages, setTotalPages] = useState(1);
-  const [page, setPage] = useState(1);
   const getModerators = async () => {
     setLoading(true);
     try {
-      const { data } = await api.get(`/getMediators?page=${page}`);
+      const { data } = await api.get("/getMediators");
       console.log(data);
-      //   setModerators(data.mediators);
-      //   setTotalPages(data.totalPages);
+      setModerators(data.result);
     } catch (err) {
       console.log(err);
       myToast(err?.response?.data?.error || "Something went wrong", "failure");
@@ -44,26 +41,22 @@ const ModeratorList = () => {
   return (
     <div>
       <div className="flex justify-between items-center mb-8">
-        <h2>All Questions</h2>
+        <h2>All Moderators</h2>
         <Link to="./add-moderator">
           <Button>Add Moderator</Button>
         </Link>
       </div>
       <div>
-        <Table
-          tHead={["S.No.", "Name", "User Id", "Created At", "Action"]}
-          loading={loading}
-        >
+        <Table tHead={["S.No.", "Name", "User Id", "Action"]} loading={loading}>
           {moderators.length > 0 ? (
             moderators.map((item, index) => (
               <tr key={item._id}>
                 <td>{index + 1}</td>
                 <td>{item.name}</td>
                 <td>{item.userId}</td>
-                <td>{item.createdAt}</td>
                 <td>
                   <span
-                    className="cursor-pointer p-4"
+                    className="cursor-pointer"
                     onClick={() => handleDelete(item._id)}
                   >
                     <RiDeleteBinLine />
@@ -79,7 +72,6 @@ const ModeratorList = () => {
             </tr>
           )}
         </Table>
-        <Pagination lastPage={totalPages} page={page} setPage={setPage} />
       </div>
     </div>
   );
