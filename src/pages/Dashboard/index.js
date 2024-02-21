46b496abe3f45476aaa14136a7c96c8d6fd09981
Ashutosh <ashutoshks@digitalnightowl.in>
2024-02-21 14:33:31 +0530

    add charts to dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import api from "../../components/utils/api";
 import myToast from "../../components/utils/myToast";
 import Card from "../../components/common/Card";
+import UsersChart from "./components/UsersChart";
+import TestsChart from "./components/TestsChart";
 
 const Dashboard = () => {
   const [loading, setLoading] = useState(true);
@@ -15,10 +17,7 @@ const Dashboard = () => {
       setDashboardData(data);
     } catch (err) {
       console.log(err);
-      myToast(
-        err?.response?.data?.error || "Something went wrong",
-        "failure"
-      );
+      myToast(err?.response?.data?.error || "Something went wrong", "failure");
     }
     setLoading(false);
   };
@@ -32,14 +31,42 @@ const Dashboard = () => {
         {loading ? (
           <div>Loading...</div>
         ) : (
-          <div className="flex gap-x-8 gap-y-4 flex-wrap">
-            {Object.keys(dashboardData).map((key) => (
-              <Card key={key} className="bg-white w-48">
-                <h5 className="capitalize mb-2">{key}</h5>
-                <h3 className="text-center">{dashboardData[key]}</h3>
+          <>
+            <div className="flex gap-x-8 gap-y-4 flex-wrap">
+              {Object.keys(dashboardData).map((key) => (
+                <Card key={key} className="bg-white w-48">
+                  <h5 className="capitalize mb-2">{key}</h5>
+                  <h3 className="text-center">{dashboardData[key]}</h3>
+                </Card>
+              ))}
+            </div>
+            <div className="mt-10 flex items-center gap-10 flex-col lg:flex-row">
+              <Card size="graph" className="bg-white h-[20rem] w-full">
+                <UsersChart
+                  data={[
+                    {
+                      name: "Users",
+                      total: dashboardData.users || 0,
+                      verified: dashboardData.verifiedUsers || 0,
+                      unverified: dashboardData.unverifiedUsers || 0,
+                    },
+                  ]}
+                />
               </Card>
-            ))}
-          </div>
+              <Card size="graph" className="bg-white h-[20rem] w-full">
+                <TestsChart
+                  data={[
+                    {
+                      name: "Tests",
+                      total: dashboardData.tests || 0,
+                      completed: dashboardData.completedTests || 0,
+                      uncompleted: dashboardData.uncompletedTests || 0,
+                    },
+                  ]}
+                />
+              </Card>
+            </div>
+          </>
         )}
       </div>
     </div>
